cc10d1153df38bcc5542dec7650007fb7a4ec21a
gaudesp <gaudespro@gmail.com>
2024-09-30 14:29:56 +0200

    feat: improve seeds

--- seeds/game.py
+++ seeds/game.py
@@ -1,12 +1,40 @@
 from seeds.dependencies import print_model_details
 
-def seed_games(db, model, count):
-  print(f"Seeding {count} games :")
-  for i in range(1, count+1):
+data = [
+  {
+    "name": "Waven",
+    "igdb_image": "co6y8e",
+    "igdb_id": "109536"
+  },
+  {
+    "name": "League of Legends",
+    "igdb_image": "co49wj",
+    "igdb_id": "115"
+  },
+  {
+    "name": "Artisan TD",
+    "igdb_image": "co7l8r",
+    "igdb_id": "281796"
+  },
+  {
+    "name": "Minecraft: Java Edition",
+    "igdb_image": "co8fu6",
+    "igdb_id": "121"
+  },
+  {
+    "name": "Palworld",
+    "igdb_image": "co7n02",
+    "igdb_id": "151665"
+  }
+]
+
+def seed_games(db, model, data=data):
+  print(f"Seeding {len(data)} games :")
+  for item in data:
     game = model(
-      name=f"Waven",
-      igdb_image="co6y8e",
-      igdb_id=109536
+      name=item['name'],
+      igdb_image=item['igdb_image'],
+      igdb_id=item['igdb_id']
     )
     db.add(game)
     print_model_details(game)
